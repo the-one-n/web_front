2ee852fe0f03e1f1494b0307b6f64bee6e3d84df
pvrvd0x <d3m35s1ah@protonmail.com>
2021-09-13 19:39:59 +0300

    Пофиксил ебаный слайдер

---
+++ js/preloader.js
@@ -0,0 +1,23 @@
+(function() {
+    const helloPreloader = document.querySelector(".hellopreloader_preload");
+
+    function fadeOut(el) {
+        el.style.opacity = 1;
+        const interHelloPreloader = setInterval(
+            function() {
+                el.style.opacity = el.style.opacity - 0.05;
+
+                if (el.style.opacity <= 0.05) {
+                    clearInterval(interHelloPreloader);
+                    helloPreloader.style.display = "none";
+                }
+                },16);
+    }
+
+    window.onload = function() {
+        setTimeout(
+            function() {
+                fadeOut(helloPreloader);
+                },1000);
+    };
+})();
