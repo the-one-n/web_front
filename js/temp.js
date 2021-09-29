809615701c1695e637b1f7ababf3aa03909b3d2f
pvrvd0x <d3m35s1ah@protonmail.com>
2021-09-29 10:40:02 +0300

    Убрал грусный из скрипта

--- js/temp.js
+++ js/temp.js
@@ -2,32 +2,29 @@ $(document).ready(function() {
     const $productPopUp = $('.product-pop-up');
     const $productShadowBox = $('.product-pop-up__shadowbox');
     const $productPopUpMenu = $('.product-pop-up-menu');
-    const $sublistItems = $('.submenu-sublist-item');
-    const $submenuSublistItemLink = $('.submenu-sublist-item__link');
+    const $menuItem = $('.submenu-sublist-item, .submenu-item');
     const $productPopUpClose = $('.product-pop-up__close');
 
     function openPopUp($target) {
-        let $children = $target.children('.product-menu').clone();
+      let $children = $target.children('.product-menu').clone();
 
+      console.log($children);
+
+      if (!!$.trim($children.html())) {
         $productPopUp.addClass('product-pop-up--active');
 
         $productPopUpMenu.append($children);
+      }
     }
 
-    $sublistItems.click((e) => {
-        const $target = $(e.target);
+    $menuItem.click((e) => {
+      const $target = $(e.currentTarget);
 
-        openPopUp($target);
-    });
-
-    $submenuSublistItemLink.click((e) => {
-        const $target = $(e.target).parent();
-
-        openPopUp($target);
+      openPopUp($target);
     });
 
     $($productPopUpClose, $productShadowBox).click(() => {
-        $productPopUpMenu.html('');
-        $productPopUp.removeClass('product-pop-up--active');
+      $productPopUpMenu.html('');
+      $productPopUp.removeClass('product-pop-up--active');
     });
-});
+  });
